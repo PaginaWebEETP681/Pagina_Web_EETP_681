e35e0a1cb8fb903d269e8ee4baefd52898d8e6e4
Pagina Web EETP/Cantina Karting Juventud <paginawebeetp@gmail.com>
2024-09-06 20:50:03 -0300

    Add files via upload

--- galeriadeff.js
+++ galeriadeff.js
@@ -26,3 +26,17 @@ document.addEventListener('DOMContentLoaded', function () {
       overlayContent.innerHTML = '';
   });
 });
+
+var regresar = $("#irArriba");
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 400){
+            regresar.addClass("visible");
+        }else{
+            regresar.removeClass("visible");
+        }
+    });
+    regresar.on("click", function(evento) {
+        evento.preventDefault();
+        $("html, body").animate({scrollTop: 0}, 500);
+
+    })
